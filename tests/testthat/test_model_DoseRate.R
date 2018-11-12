c55a9a5433360f1c3f8e724088081ce8114813e4
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-11-12 14:24:02 +0100

    Add tests

---
+++ tests/testthat/test_model_DoseRate.R
@@ -0,0 +1,23 @@
+context("Test model_DoseRate()")
+
+test_that("Full function test", {
+  testthat::skip_on_cran()
+
+  ##load Example dataset
+  data("Example_Data", envir = environment())
+
+  ##break function
+  expect_error(model_DoseRate(data = "test"), regexp = "'data' is not a 'data.frame'")
+  expect_error(model_DoseRate(data = data.frame(x = 1)), regexp = "The column names of your input data.frame do not match the requirements.")
+  expect_warning(model_DoseRate(data = Example_Data[23,], n.MC = 10),regexp = "Extrem case: DE > max cumulative dose rate!")
+
+  ##run simple example
+  expect_type(model_DoseRate(data = Example_Data[14,],
+    n.MC = 2,
+    txtProgressBar = FALSE
+  ), type = "list")
+
+
+
+
+})
